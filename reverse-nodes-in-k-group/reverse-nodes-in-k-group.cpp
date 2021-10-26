3e397d6d7de8b30f9545c328d53eb144792df5e2
Noble Joy <noblejoy20@gmail.com>
2021-10-26 21:33:37 +0530

    Update reverse-nodes-in-k-group.cpp

--- reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -11,14 +11,16 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
+        // Initialise pointers same as single linked list reversal.
         ListNode* curr=head;
         ListNode* prev=NULL;
         ListNode* nxt;
         
+        // check whether node are of length k else return head
         ListNode* check=head;
-        int count=1;
+        int count=0;
         
-        while(check->next!=NULL && count<=k){
+        while(check!=NULL && count<k){
             check=check->next;
             count++;
         }
@@ -27,8 +29,11 @@ public:
             return head;
         }
         
+        
+        //Initialise counter back to zero.
         count=0;
         
+        //Do single LL reverse upto k nodes
         while(curr!=NULL && count<k){
             nxt=curr->next;
             curr->next=prev;
@@ -36,10 +41,13 @@ public:
             curr=nxt;
             count++;
         }
+        
+        //if the k+1 node is not null do recursive call.
         if(nxt!=NULL){
             head->next=reverseKGroup(nxt,k);
         }
         
+        //return the prev node.
         return prev;
         
     }
