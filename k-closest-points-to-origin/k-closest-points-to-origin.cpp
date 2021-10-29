fcd2c6c7247a877a5185e4eced38ef005c443f5c
Noble Joy <noblejoy20@gmail.com>
2021-10-29 21:34:31 +0530

    Time: 172 ms (77.54%), Space: 70.3 MB (19.27%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
+    int partition(vector<vector<int>>& result,int pivot,int lo,int hi){
+        int i=lo,j=lo;
+        while(i<=hi){
+            if(result[i][0]<=pivot){
+                swap(result[i],result[j]);
+                i++;j++;
+            }else{
+                i++;
+            }
+        }
+        return j-1;
+    }
+    int quickSelect(vector<vector<int>>& result,int lo,int hi,int k){
+        if(lo<=hi){
+            int pi=partition(result,result[hi][0],lo,hi);
+            if(pi==k){
+                return pi;
+            }else if(k<pi){
+                return quickSelect(result,lo,pi-1,k);
+            }else{
+                return quickSelect(result,pi+1,hi,k);
+            }
+        }
+        return hi+1;
+    }
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k, int count=0) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        vector<vector<int>> result;
+        vector<vector<int>> result,ans;
         for(auto point:points){
-            pq.push({abs((point[0]-0)*(point[0]-0)+(point[1]-0)*(point[1]-0)),count++});
+            result.push_back(vector<int>({abs((point[0]-0)*(point[0]-0)+(point[1]-0)*(point[1]-0)),count++}));
         }
-        count=0;
-        while(!pq.empty() && count<k){
-            auto ans=pq.top();pq.pop();
-            result.push_back(points[ans.second]);
-            count++;
+        int pos=quickSelect(result,0,result.size()-1,k);
+        for(int i=0;i<pos;i++){
+            ans.push_back(points[result[i][1]]);
         }
-        return result;
+        return ans;
     }
 };
