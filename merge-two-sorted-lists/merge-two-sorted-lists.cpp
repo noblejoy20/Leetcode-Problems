f081a3075d14b85afefed64bbc27c42bb23c7464
Noble Joy <noblejoy20@gmail.com>
2021-11-01 07:39:17 +0530

    Time: 12 ms (40.42%), Space: 14.9 MB (17.28%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,40 +11,31 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-       ListNode* head;
-        ListNode* temp;
-        
-        if(l1==NULL&&l2==NULL){
-            return NULL;
-        }else if(l1==NULL){
-            return l2;
-        }else if(l2==NULL){
-            return l1;
-        }
-        if(l1->val<=l2->val){
+        ListNode* temp=NULL;
+        ListNode* head=temp;
+        if(!l1) return l2;
+        if(!l2) return l1;
+        if(l1->val<l2->val){
             temp=l1;
-            l1=temp->next;
+            head=l1;
+            l1=l1->next;
         }else{
             temp=l2;
-            l2=temp->next;
+            head=l2;
+            l2=l2->next;
         }
-        head=temp;
-         while(l1!=NULL&&l2!=NULL){
-            if(l1->val<=l2->val){
+        while(l1&&l2){
+            if(l1->val<l2->val){
                 temp->next=l1;
                 temp=l1;
                 l1=l1->next;
-                continue;
-            }
-            if(l1->val>=l2->val){
+            }else{
                 temp->next=l2;
                 temp=l2;
                 l2=l2->next;
-                continue;
             }
         }
-    
-        if(l1==NULL){
+        if(!l1){
             temp->next=l2;
         }else{
             temp->next=l1;
