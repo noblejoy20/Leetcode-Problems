6a8b183e1073ab8c16e1360a89c5e780f3c8fcc5
Noble Joy <noblejoy20@gmail.com>
2022-01-04 09:08:17 +0530

    Time: 2 ms (12.33%), Space: 6 MB (5.99%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        return n?n ^ (int)(pow(2,(int)(log2(n)+1))-1):1;
+        return n?n ^ (int)pow(2,(int)(log2(n)+1))-1:1;
     }
 };
