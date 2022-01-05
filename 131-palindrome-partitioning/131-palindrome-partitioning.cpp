2d33ec14291d37fc50dfb7e1376709a933a3f7f5
Noble Joy <noblejoy20@gmail.com>
2022-01-05 13:34:37 +0530

    Time: 172 ms (37.33%), Space: 79.9 MB (37.58%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     vector<vector<string>> ans;
-    bool isPalin(string s){
-        int i=0,j=s.length()-1;
-        while(i<j){
+    bool isPalin(string s,int i,int j){
+        while(i<=j){
             if(s[i++]!=s[j--]){
                 return false;
             }
         }
         return true;
     }
-    void solve(string s,vector<string>& res){
-      if(s==""){
+    void solve(string& s,vector<string>& res,int l,int r){
+      if(l==r){
           ans.push_back(res);
           return;
       }
-      for(int k=1;k<=s.length();k++)
+      for(int k=l;k<r;k++)
       {
-          string temp=s.substr(0,k);
-          if(isPalin(temp)){
-              res.push_back(temp);
-              solve(s.substr(k),res);
+          if(isPalin(s,l,k)){
+              res.push_back(s.substr(l,k-l+1));
+              solve(s,res,k+1,r);
               res.pop_back();
           }
       } 
     }
     vector<vector<string>> partition(string s) {
         vector<string> res;
-        solve(s,res);
+        solve(s,res,0,s.length());
         return ans;
     }
 };
