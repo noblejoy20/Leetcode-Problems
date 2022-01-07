bc6b3306ca29d32c7bc7eaaecc912795d836b66c
Noble Joy <noblejoy20@gmail.com>
2022-01-07 10:27:00 +0530

    Time: 40 ms (9.07%), Space: 16.6 MB (48.28%) - LeetHub

--- 382-linked-list-random-node/382-linked-list-random-node.cpp
+++ 382-linked-list-random-node/382-linked-list-random-node.cpp
@@ -11,23 +11,26 @@
 class Solution {
 public:
     ListNode* first;
-    
     Solution(ListNode* head) {
         srand(time(NULL));
         first=head;
     }
     
     int getRandom() {
-        int ans=first->val;
-        ListNode* ptr=first->next;
-        int count=2;
-        while(ptr!=NULL){            
-            int n=rand()%count;
-            if(n==0){
+        ListNode* ptr=first;
+        if(!ptr->next){
+            return ptr->val;
+        }
+        int ans=ptr->val;
+        int count=0;
+        while(ptr!=NULL){
+            count++;
+            int n=(rand()%count)+1;
+            if(n==count){
                 ans=ptr->val;
+
             }
             ptr=ptr->next;
-            count++;
         }
         return ans;
     }
