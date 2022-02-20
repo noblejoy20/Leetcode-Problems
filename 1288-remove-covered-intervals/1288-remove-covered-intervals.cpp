e52a3691d233d4e85c6f35f4d24aa8043398241f
Noble Joy <noblejoy20@gmail.com>
2022-02-20 09:55:59 +0530

    Time: 24 ms (79.82%), Space: 12.1 MB (18.20%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+    int removeCoveredIntervals(vector<vector<int>>& intervals,int count = 1) {
         stack<vector<int>> stk;
         sort(intervals.begin(),intervals.end(),[&](vector<int>& a,vector<int>& b){
             return a[1]==b[1]?a[0]>b[0]:a[1]<b[1];
         });
-        for(int i=intervals.size()-1;i>=0;i--){
-            if(!stk.empty()){
-                auto temp = stk.top();
-                if(temp[0]>intervals[i][0]||temp[1]<intervals[i][1]){
-                    stk.push(intervals[i]);
-                }
+        for(int i=intervals.size()-2;i>=0;i--){
+            auto temp = intervals[i+1];
+            if(temp[0]>intervals[i][0]||temp[1]<intervals[i][1]){
+                count++;
             }else{
-                stk.push(intervals[i]);
-            }
+                intervals[i][0] = temp[0];
+                intervals[i][1] = temp[1];
+            }  
         }
-        return stk.size();
+        return count;
     }
 };
