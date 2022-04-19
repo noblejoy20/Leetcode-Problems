0766cc4fdc140c3e24a755f23e0dfd61dc1b2b9c
Noble Joy <noblejoy20@gmail.com>
2022-04-19 11:09:21 +0530

    Time: 57 ms (28.62%), Space: 57.9 MB (70.41%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -11,67 +11,23 @@
  */
 class Solution {
 public:
-    TreeNode* temp=NULL;
-    TreeNode* temp1=NULL;
-    void findRight(TreeNode* root,int val){
+    TreeNode* first=NULL;
+    TreeNode* second=NULL;
+    TreeNode* prev=new TreeNode(INT_MIN);
+    void solve(TreeNode* root){
         if(!root) return;
-        if(root->val<val){
-            if(temp){
-                if(temp->val>root->val){
-                    temp=root;
-                }
-            }else{
-                temp=root;
-            }
+        solve(root->left);
+        if(first==NULL && prev->val>root->val){     
+            first=prev;
         }
-        findRight(root->left,val);
-        findRight(root->right,val);
-    }
-    void findLeft(TreeNode* root,int val){
-        if(!root) return;
-        if(root->val>val){
-            if(temp1){
-                if(temp1->val<root->val){
-                    temp1=root;
-                }
-            }else{
-                temp1=root;
-            }
+        if(first!=NULL && prev->val>root->val){
+            second=root;
         }
-        findLeft(root->left,val);
-        findLeft(root->right,val);
-    }
-    bool solve(TreeNode* root){
-        if(!root) return false;
-        temp1=NULL;
-        findLeft(root->left,root->val);
-        
-        temp=NULL;
-        findRight(root->right,root->val);
-
-        if(temp && temp1){
-            swap(temp->val,temp1->val);
-            return true;
-        }
-        else if(temp1){
-            swap(root->val,temp1->val);
-            return true;
-        }
-        else if(temp){
-            
-            swap(root->val,temp->val);
-            return true;
-        }
-         if(solve(root->left)){
-             return true;
-         }
-        if(solve(root->right)){
-            return true;
-        }
-        return false;
+        prev=root;
+        solve(root->right);
     }
     void recoverTree(TreeNode* root) {
-        solve(root);        
+        solve(root);
+        swap(first->val,second->val);
     }
-    
 };
