4c7b415c9503979202e6bb09ea0a03ae3e08a998
Noble Joy <noblejoy20@gmail.com>
2022-04-22 11:12:26 +0530

    Time: 175 ms (60.55%), Space: 56.4 MB (58.31%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,40 +1,57 @@
 struct LinkedList{
-    int key;
-    int value;
-    LinkedList* next;
-    LinkedList* prev;
-    
-    LinkedList(){
-       next=NULL;
-       prev=NULL;
-    }
-    
+    int key,value;
+    LinkedList *next,*prev;
+       
     LinkedList(int k,int v){
         key=k;
         value=v;
-        next=NULL;
-        prev=NULL;
+        next=prev=NULL;
     }
 };
 class MyHashMap {
 private:
     vector<LinkedList*> nodes;
+    const int bucket_size=1000;
 public:
+   
     MyHashMap() {
-        for(int i=0;i<1000;i++){
+        Init(nodes);
+    }
+    
+     void Init(vector<LinkedList*>& nodes){
+        for(int i=0;i<bucket_size;i++){
             LinkedList* head = new LinkedList(-1,-1);
             LinkedList* tail = new LinkedList(-1,-1);
             head->next=tail;
             tail->prev=head;
             nodes.push_back(head);
         }
-        //nodes.resize(1000,NULL);
     }
     
-    void put(int key, int value) {
-        int pos = key % 1000;
-  
-        LinkedList* ptr=nodes[pos];
+    void insert(LinkedList *ptr,int key,int value){
+        LinkedList* temp = new LinkedList(key,value);
+        temp->next=ptr->next;
+        ptr->next=temp;
+        temp->prev=temp->next->prev;
+        temp->next->prev=temp;
+    }
+    
+     int findKey(LinkedList *ptr,int key){
+        while(ptr->next!=NULL){
+            if(ptr->key==key){ return ptr->value;}
+            ptr=ptr->next;
+        }
+        if(ptr->key==key){ return ptr->value;}
+        return -1;
+    }
+    
+    void deleteKey(LinkedList* ptr){
+        ptr->prev->next=ptr->next;
+        ptr->next->prev=ptr->prev;
+    }
+    
+    void put(int key, int value) { 
+        LinkedList* ptr=nodes[key % bucket_size];
         while(ptr->next!=NULL && ptr->next->key!=-1){
             if(ptr->key == key){
                 ptr->value = value;
@@ -45,41 +62,28 @@ public:
         if(ptr->key == key){
             ptr->value = value;
             return;
-        }            
-
-        LinkedList* temp = new LinkedList(key,value);
-        temp->next=ptr->next;
-        ptr->next=temp;
-        temp->prev=temp->next->prev;
-        temp->next->prev=temp;
-        
+        }                   
+        insert(ptr,key,value);               
     }
     
+   
     int get(int key) {
-        int pos = key % 1000;
-        LinkedList* ptr = nodes[pos];
-        while(ptr->next!=NULL){
-            if(ptr->key==key){ return ptr->value;}
-            ptr=ptr->next;
-        }
-        if(ptr->key==key){ return ptr->value;}
-        return -1;
+        LinkedList* ptr = nodes[key % bucket_size];
+        int val = findKey(ptr,key);
+        return val;
     }
     
     void remove(int key) {
-        int pos = key % 1000;
-        LinkedList* ptr = nodes[pos];
+        LinkedList* ptr = nodes[key % bucket_size];
         while(ptr->next!=NULL){        
             if(ptr->key==key){    
-                ptr->prev->next=ptr->next;
-                ptr->next->prev=ptr->prev;
+                deleteKey(ptr);
                 return;
             }
             ptr=ptr->next;
         }
         if(ptr->key==key){    
-            ptr->prev->next=ptr->next;
-            ptr->next->prev=ptr->prev;
+            deleteKey(ptr);
             return;
         }  
     }
