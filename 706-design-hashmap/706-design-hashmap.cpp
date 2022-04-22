36fdd1f5313dae5293ff262382c5e41f10ab5d33
Noble Joy <noblejoy20@gmail.com>
2022-04-22 10:45:40 +0530

    Time: 127 ms (87.16%), Space: 53.7 MB (63.90%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -0,0 +1,98 @@
+struct LinkedList{
+    int key;
+    int value;
+    LinkedList* next;
+    LinkedList* prev;
+    
+    LinkedList(){
+       next=NULL;
+       prev=NULL;
+    }
+    
+    LinkedList(int k,int v){
+        key=k;
+        value=v;
+        next=NULL;
+        prev=NULL;
+    }
+};
+class MyHashMap {
+private:
+    vector<LinkedList*> nodes;
+public:
+    MyHashMap() {
+        nodes.resize(1000,NULL);
+    }
+    
+    void put(int key, int value) {
+        int pos = key % 1000;
+        if(nodes[pos] == NULL){
+            LinkedList* head = new LinkedList(-1,-1);
+            LinkedList* tail = new LinkedList(-1,-1);
+            head->next=tail;
+            tail->prev=head;
+            nodes[pos] = head;
+        }else{  
+            LinkedList* ptr=nodes[pos];
+            while(ptr->next!=NULL && ptr->next->key!=-1){
+                if(ptr->key == key){
+                    ptr->value = value;
+                    return;
+                }
+                ptr=ptr->next;
+            }
+            if(ptr->key == key){
+                ptr->value = value;
+                return;
+            }            
+        }
+        LinkedList* ptr=nodes[pos];
+        LinkedList* temp = new LinkedList(key,value);
+        temp->next=ptr->next;
+        ptr->next=temp;
+        temp->prev=temp->next->prev;
+        temp->next->prev=temp;
+        
+    }
+    
+    int get(int key) {
+        int pos = key % 1000;
+        if(nodes[pos]!=NULL){
+            LinkedList* ptr = nodes[pos];
+            while(ptr->next!=NULL){
+                if(ptr->key==key){ return ptr->value;}
+                ptr=ptr->next;
+            }
+            if(ptr->key==key){ return ptr->value;}
+        }
+        return -1;
+    }
+    
+    void remove(int key) {
+        int pos = key % 1000;
+        if(nodes[pos]!=NULL){
+            LinkedList* ptr = nodes[pos];
+            while(ptr->next!=NULL){        
+                if(ptr->key==key){    
+                    ptr->prev->next=ptr->next;
+                    ptr->next->prev=ptr->prev;
+                    return;
+                }
+                ptr=ptr->next;
+            }
+            if(ptr->key==key){    
+                ptr->prev->next=ptr->next;
+                ptr->next->prev=ptr->prev;
+                return;
+            }
+        }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
