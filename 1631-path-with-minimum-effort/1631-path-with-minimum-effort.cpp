f303aedebc66fcbd9735e7672bf4421d1132213d
Noble Joy <noblejoy20@gmail.com>
2022-04-28 09:36:25 +0530

    Time: 1409 ms (5.00%), Space: 49.5 MB (29.85%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,36 +1,58 @@
 typedef pair<int,pair<int,int>> pp;
 class Solution {
 private:
-    priority_queue<pp,vector<pp>,greater<pp>> q;
+    
     int x[4]={1,0,-1,0};
     int y[4]={0,1,0,-1};
 public:
-    int minimumEffortPath(vector<vector<int>>& grid) {
+    bool bfs(vector<vector<int>>& grid,int threshold){
         int m = grid.size();
         int n = grid[0].size();
+        int mn=0;
+        priority_queue<pp,vector<pp>,greater<pp>> q;
         
-        vector<vector<int>> dist(m,vector<int>(n,INT_MAX));
+        vector<vector<int>> vis(m,vector<int>(n,false));
         q.push({0,{0,0}});
+        vis[0][0]=true;
         
         while(!q.empty()){
             auto it=q.top();q.pop();
-            int val = it.first;
             int row = it.second.first;
             int col = it.second.second;
-            if(val>dist[row][col]) continue;
-            if(row == m-1 && col == n-1) return val;
+            if(row == m-1 && col == n-1) return true;
             int curr=grid[row][col];
             for(int i=0;i<4;i++){
-                if(row+x[i]>=0 && row+x[i]<m && col+y[i]>=0 && col+y[i]<n){
+                if(row+x[i]>=0 && row+x[i]<m && col+y[i]>=0 && col+y[i]<n && !vis[row+x[i]][col+y[i]]){       
                     int next = grid[row+x[i]][col+y[i]];
-                    int mn =max(val,abs(next-curr));
-                    if(dist[row+x[i]][col+y[i]]>mn){
-                        dist[row+x[i]][col+y[i]]=mn;
-                        q.push({dist[row+x[i]][col+y[i]],{row+x[i],col+y[i]}});
+                    if(abs(next-curr)<=threshold){
+                        vis[row+x[i]][col+y[i]]=true;
+                        q.push({abs(next-curr),{row+x[i],col+y[i]}});
                     }
                 }
             }
         }
-        return -1;
+        return false;
+    }
+    int minimumEffortPath(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int low=0,high = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                high = max(high,grid[i][j]);
+            }
+        }
+        int ans=0;
+        while(low<=high){
+            int mid = low +(high-low)/2;
+            if(bfs(grid,mid)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        
+        return ans;
     }
 };
